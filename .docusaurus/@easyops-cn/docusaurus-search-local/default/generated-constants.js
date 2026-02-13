e67e63ff31fcdde6ba186c7c7751af295505c870
Alexander Fandos <alexander.fandos@midokura.com>
2026-02-13 14:48:20 +0100

    refactor[docusarus]: remove .docusarus folder from repo (#79)
    the `.docusarus` is listed to ignore by the `.gitignore` file. However,
    as the files were already commited git still tracked them.

--- .docusaurus/@easyops-cn/docusaurus-search-local/default/generated-constants.js
+++
@@ -1,6 +0,0 @@
-import lunr from "/Users/jim/Desktop/github/phoenix-documentation/node_modules/lunr/lunr.js";
-export const removeDefaultStopWordFilter = [];
-export const language = ["en"];
-export const searchIndexUrl = "search-index{dir}.json?_=e335e50d";
-export const searchResultLimits = 8;
-export const fuzzyMatchingDistance = 1;
